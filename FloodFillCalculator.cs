9e91769a229067bcf3867bb7f9a973fda7a85f38
Mathias Fritsch <Mathias.Fritsch@senacor.com>
2022-12-04 21:26:47 +0100

    check tight space

---
+++ FloodFillCalculator.cs
@@ -0,0 +1,63 @@
+﻿using BattlesnakeAzureFunction.Model;
+using System.Collections.Generic;
+
+namespace BattlesnakeAzureFunction
+{
+    public class FloodFillCalculator
+    {
+        private enum FloodFillState
+        { notVisited, line, filled }
+
+        private FloodFillState[,] board;
+        private int max;
+        private int filledCounter = 0;
+        private int size = 0;
+
+        public FloodFillCalculator(int size, Coord[] snake, int max)
+        {
+            this.max = max;
+            this.board = new FloodFillState[size, size];
+            this.size = size;
+
+            foreach (var snakeItem in snake)
+            {
+                this.board[snakeItem.X, snakeItem.Y] = FloodFillState.line;
+            }
+        }
+
+        public int FloodFill(Coord position)
+        {
+            var allDirections = new List<Direction> { Direction.left,
+                Direction.right,
+                Direction.up,
+                Direction.down };
+
+            if (this.filledCounter >= max) return filledCounter;
+
+            if (this.board[position.X, position.Y] == FloodFillState.line ||
+                this.board[position.X, position.Y] == FloodFillState.filled
+                )
+            {
+                return filledCounter;
+            }
+
+            if (this.board[position.X, position.Y] == FloodFillState.notVisited)
+            {
+                this.board[position.X, position.Y] = FloodFillState.filled;
+                this.filledCounter++;
+                if (this.filledCounter >= max) return filledCounter;
+
+                foreach (var direction in allDirections)
+                {
+                    var newPosition = position.Move(direction);
+                    if (newPosition.X < 0 || newPosition.X >= size
+                        || newPosition.Y < 0 || newPosition.Y >= size) continue;
+
+                    FloodFill(position.Move(direction));
+                }
+            }
+
+            return filledCounter;
+        }
+    }
+}
--- SnakeProcessor.cs
+++ SnakeProcessor.cs
@@ -10,10 +10,17 @@ namespace BattlesnakeAzureFunction
         private List<Direction> allowedDirections;
         private readonly GameState gameState;
 
+        private readonly List<Direction> allDirections = new List<Direction> {
+                Direction.left,
+                Direction.right,
+                Direction.up,
+                Direction.down};
+
         public SnakeProcessor(GameState gameState)
         {
             this.gameState = gameState;
-            this.allowedDirections = new List<Direction> { Direction.left,
+            this.allowedDirections = new List<Direction> {
+                Direction.left,
                 Direction.right,
                 Direction.up,
                 Direction.down };
@@ -25,7 +32,8 @@ namespace BattlesnakeAzureFunction
 
             foreach (var possibleDirection in allowedDirections)
             {
-                if (gameState.Board.MoveStayOnBoad(possibleDirection, gameState.You.Head)) nonOffboardDirections.Add(possibleDirection);
+                if (gameState.Board.MoveStayOnBoad(possibleDirection, gameState.You.Head))
+                    nonOffboardDirections.Add(possibleDirection);
             }
             allowedDirections = nonOffboardDirections;
             return this;
@@ -45,6 +53,38 @@ namespace BattlesnakeAzureFunction
             allowedDirections = nonSelfDirections;
             return this;
         }
+
+        public SnakeProcessor DontGoToTightSpace()
+        {
+            int minimumSpace = 3;
+
+            var bigSpaceDirections = new List<Direction>();
+
+            foreach (var possibleDirection in allowedDirections)
+            {
+                var snake = gameState.You.Body.Distinct().ToList();
+
+                var headAfterMove = gameState.You.Head.Move(possibleDirection);
+                snake.Add(headAfterMove);
+
+                int bestSize = 0;
+                foreach (var directionForFloodFill in allDirections)
+                {
+                    var ffc = new FloodFillCalculator(gameState.Board.Width,
+                          snake.ToArray(),
+                          minimumSpace);
+
+                    int size = ffc.FloodFill(headAfterMove.Move(directionForFloodFill));
+                    bestSize = size > bestSize ? size : bestSize;
+                }
+                if(bestSize >= minimumSpace)
+                bigSpaceDirections.Add(possibleDirection);
+            }
+
+            this.allowedDirections = bigSpaceDirections;
+            return this;
+        }
+
         public Direction GetMove()
         {
             if (allowedDirections.Any())
